773c3c3a8b5ba1da4618ab6e4ad471546b2591d5
codytheking <codytheking93@gmail.com>
2022-05-19 08:26:27 -0700

    Solution

---
+++ Solution/Person.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+public class Person {
+    private String name;
+    private int age;
+    private int count;
+
+    public Person() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
